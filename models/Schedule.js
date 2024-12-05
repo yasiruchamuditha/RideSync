c1d55d1f9e469f69fd4829c7840ca89a25df5951
Yasiru Chamuditha <109359904+yasiruchamuditha@users.noreply.github.com>
2024-12-06 00:13:36 +0530

    create api/schedule

---
+++ models/Schedule.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+import { Schema } from 'mongoose';
+
+const ScheduleSchema = new Schema({
+    busId: {
+        type: Schema.Types.ObjectId,
+        ref: "Bus",
+    },
+    busRouteType: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    startCity: {
+        type: String,
+        required: true,
+    },
+    endCity: {
+        type: String,
+        required: true,
+    },
+    estimatedTime: {
+        type: String,
+        required: true,
+    },
+    ticketPrice: {
+        type: Number,
+        required: true,
+    },
+    availableSeats: {
+        type: Number,
+        default: 50,
+        required: true,
+    },
+    seatLayout: [
+        {
+            seatNumber: String,
+            bookedBy: { type: Schema.Types.ObjectId, ref: "User", default: null },
+            position: String,
+        }
+    ],
+}, { timestamps: true });
+
+export default mongoose.model('Schedule', ScheduleSchema);
