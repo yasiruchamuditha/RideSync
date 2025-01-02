696ff073aa694f1c57ac0ecc20fa6da1acbbf837
Yasiru Chamuditha <109359904+yasiruchamuditha@users.noreply.github.com>
2025-01-03 01:08:50 +0530

    Add custom CORS middleware to allow specific origin and methods

--- app.js
+++ app.js
@@ -52,6 +52,14 @@ const app = express();
 
 // app.use(cors()); // Enable CORS
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://bus-ride-sync.vercel.app'); // Override any defaults
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 // Middleware
 app.use(express.json()); // Handle JSON data
 app.use(cookieParser()); // Handle cookies
