d64cbfea590c3af3991da09695e3623b1dfd55e4
Yasiru Chamuditha <109359904+yasiruchamuditha@users.noreply.github.com>
2025-01-03 00:58:38 +0530

    Comment out CORS middleware configuration in app.js

--- app.js
+++ app.js
@@ -36,19 +36,19 @@ connectDB();
 const app = express();
 
 // Enable CORS for your frontend application
-app.use(
-  cors({
-    // origin: [
-    //   // 'http://localhost:3000',
-    //   'https://bus-ride-sync.vercel.app',
-    //   'https://ridesync.yasiru.site', // Added this
-    // ],
-    // origin: true,
-    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'], // Allow specific HTTP methods
-    credentials: true, // Allow cookies and credentials
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow necessary headers
-  })
-);
+// app.use(
+//   cors({
+//     // origin: [
+//     //   // 'http://localhost:3000',
+//     //   'https://bus-ride-sync.vercel.app',
+//     //   'https://ridesync.yasiru.site', // Added this
+//     // ],
+//     // origin: true,
+//     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'], // Allow specific HTTP methods
+//     credentials: true, // Allow cookies and credentials
+//     allowedHeaders: ['Content-Type', 'Authorization'], // Allow necessary headers
+//   })
+// );
 
 // app.use(cors()); // Enable CORS
 
